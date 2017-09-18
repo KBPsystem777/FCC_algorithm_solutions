c4f6ac61e66c5d2ee72dfedfc4e4e0aa80c8f8b0
Koleen BP <koleen.bp@outlook.com>
2017-09-18 09:39:18 +0000

    Add new file

---
+++ factorialize.js
@@ -0,0 +1,18 @@
+//Return the factorial of the provided integer.
+
+//If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+//Factorials are often represented with the shorthand notation n!
+
+function factorialize(num) {
+
+var x = 1;
+    for (i=1;i<=num;i++){
+    x=x*i;
+    }
+    return x;
+    
+}
+
+
+//Example: factorialize(7)  //5040
